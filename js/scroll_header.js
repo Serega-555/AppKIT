73f6e186d47db032e8774216e07e0c1943afcd0a
liza <21zozo@mail.ru>
2025-01-03 00:32:12 +0600

    Добавил изменение цветов в navbar при скроле Добавил тайминг для смены картинов в header

--- js/scroll_header.js
+++ js/scroll_header.js
@@ -8,6 +8,12 @@ const header__MainText = document.getElementById("header__MainText");
 const header__text = document.getElementById("header__text");
 const container__leftButton = document.getElementById("container__leftButton");
 const header__image = document.getElementById("header__image");
+// контейнер navbar
+const header__navbar = document.querySelector(".header__navbar");
+// элементы navbar
+const navbar_link = document.querySelectorAll(".navbar_link");
+const header__logo = document.querySelector(".header__logo p");
+const header__logoBefore = document.querySelector(".header__logo>p::before");
 
 
 function changeContent1() {
@@ -39,7 +45,7 @@ function changeContent3() {
     header__MainText.innerHTML = "Ready to build outstanding apps?";
     header__text.innerHTML = "Get AppKit today and it will supercharge your development. It's a must-have for any developers who are serious about building great products!";
     container__leftButton.innerHTML = "Try it now";
-    container__leftButton.style.width = "142px"
+    container__leftButton.style.width = "142px";
     header__image.style.backgroundImage = `url(${"../images/hero-3.jpg"})`;
     // header__image.style.transitionDuration = ".7s"
     header__scrollButton1.style.backgroundColor = "#ffffff66";
@@ -47,8 +53,39 @@ function changeContent3() {
     header__scrollButton3.style.backgroundColor = "#40babd";
 }
 
+function changeImageHeader (){
+    changeContent1()
+    setTimeout(changeContent2, 10000);
+    setTimeout(changeContent3, 20000);
+}
+
+// функция для смены цвета меню при прокрутки
+function changeColorNavbarLink(){
+    if (window.scrollY > 56) {
+        let keysNavbarText = 0;
+        for (let i of navbar_link){
+            navbar_link[keysNavbarText].style.color = "#A2A6AF";
+            keysNavbarText++;
+        }
+        header__logo.style.color = "#494D55";
+        header__navbar.style.backgroundColor = "#FFFFFF";
+    }else{
+        let keysNavbarText = 0;
+        for (let i of navbar_link){
+            navbar_link[keysNavbarText].style.color = "#FFFFFFA6";
+            header__logo.style.color = "#FFFFFF";
+            keysNavbarText++;
+        }
+        header__navbar.style.backgroundColor = "";
+    }
+}
+
+changeImageHeader();
+setInterval(changeImageHeader, 30000);
 header__scrollButton1.addEventListener("click", changeContent1);
 header__scrollButton2.addEventListener("click", changeContent2);
 header__scrollButton3.addEventListener("click", changeContent3);
+
+document.addEventListener('scroll', changeColorNavbarLink);
 
 document.addEventListener("DOMContentLoaded", changeContent1);
